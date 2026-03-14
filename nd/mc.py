11414a3d5600fee97852b4bcde85a8916d3ae60d
Ivan Ramos Pagnossin <ivan.pagnossin@sigalei.com.br>
2026-03-14 16:18:17 -0300

    build energy spectrum from many runs

--- nd/mc.py
+++ nd/mc.py
@@ -1,7 +1,27 @@
-from dynamic import simulate
+from dynamic import simulate, spectrum
 from transitions import transitions
 
 
+def merge_spectra(s1, s2):
+    for energy, count in s2.items():
+        if energy in s1:
+            s1[energy] += count
+        else:
+            s1[energy] = count
+    return s1
+
+
+def simulate_mc():
+    n_simulations: int = 100
+
+    total_spectrum: dict = {}
+    for _ in range(n_simulations):
+        trajectory = simulate('A', transitions)
+        spectrum_ = spectrum(trajectory)
+        merge_spectra(total_spectrum, spectrum_)
+
+    print(total_spectrum)
+
+
 if __name__ == "__main__":
-    trajectories = [simulate('A', transitions) for _ in range(100)]
-    print(trajectories)
+    simulate_mc()
