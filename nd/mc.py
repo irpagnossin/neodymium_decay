f9e03d42385c036db3e75e8b34da1e3e7026c54a
Ivan Ramos Pagnossin <ivan.pagnossin@sigalei.com.br>
2026-03-14 16:04:35 -0300

    many trajectories (Monte Carlo)

--- nd/mc.py
+++ nd/mc.py
@@ -1,31 +1,7 @@
-from collections import Counter
-
-
-from transitions import transitions, energies
-from dynamic import step
-
-
-def monte_carlo_stationary(start_state, transitions, n_steps):
-    state = start_state
-    counts = Counter()
-    t_counts = Counter()
-
-    try:
-        for _ in range(n_steps):
-            counts[state] += 1
-            next_state = step(state, transitions)
-            transition_energy = energies[next_state] - energies[state]
-            t_counts[transition_energy] += 1
-            state = next_state
-    except StopIteration as e:
-        print(e)
-
-    total = sum(counts.values())
-    t_total = sum(t_counts.values())
-    return {s: counts[s]/total for s in counts}, {e: t_counts[e]/t_total for e in t_counts}
+from dynamic import simulate
+from transitions import transitions
 
 
 if __name__ == "__main__":
-    states_dist, transitions_dist = monte_carlo_stationary("A", transitions, 100_000)
-    print(states_dist)
-    print(transitions_dist)
+    trajectories = [simulate('A', transitions) for _ in range(100)]
+    print(trajectories)
