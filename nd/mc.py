6a236fc1dc5d303295e7e255e3953ea0efe3081e
Ivan Ramos Pagnossin <ivan.pagnossin@sigalei.com.br>
2026-03-14 15:49:48 -0300

    dummy example

--- nd/mc.py
+++ nd/mc.py
@@ -10,12 +10,15 @@ def monte_carlo_stationary(start_state, transitions, n_steps):
     counts = Counter()
     t_counts = Counter()
 
-    for _ in range(n_steps):
-        counts[state] += 1
-        next_state = step(state, transitions)
-        transition_energy = energies[next_state] - energies[state]
-        t_counts[transition_energy] += 1
-        state = next_state
+    try:
+        for _ in range(n_steps):
+            counts[state] += 1
+            next_state = step(state, transitions)
+            transition_energy = energies[next_state] - energies[state]
+            t_counts[transition_energy] += 1
+            state = next_state
+    except StopIteration as e:
+        print(e)
 
     total = sum(counts.values())
     t_total = sum(t_counts.values())
