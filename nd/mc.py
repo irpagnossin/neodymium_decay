5096281310291c43e8363b2d6482d31f96e2f11f
Ivan Ramos Pagnossin <ivan.pagnossin@sigalei.com.br>
2026-03-14 16:48:18 -0300

    refactor

--- nd/mc.py
+++ nd/mc.py
@@ -1,15 +1,5 @@
 from dynamic import simulate, transition_energies
-import numpy as np
 from transitions import transitions
-
-
-def merge_spectra(s1, s2):
-    for energy, count in s2.items():
-        if energy in s1:
-            s1[energy] += count
-        else:
-            s1[energy] = count
-    return s1
 
 
 def simulate_mc():
@@ -25,14 +15,14 @@ def simulate_mc():
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    import numpy as np
 
     energies = simulate_mc()
-    hist, bin_edges = np.histogram(energies, bins=50, range=(-1.1, 0), density=True)
-    print("Histogram counts:", hist)
-    print("Bin edges:", bin_edges)
+    energies = [-1 * energy for energy in energies]
+    frequencies, bin_edges = np.histogram(energies, bins=50, range=(0, 1.1), density=True)
 
-    plt.bar(bin_edges[:-1], hist, width=np.diff(bin_edges), align='edge', edgecolor='black')
-    plt.title('Histogram using np.histogram and plt.bar')
-    plt.xlabel('Value')
-    plt.ylabel('Frequency')
+    plt.bar(bin_edges[:-1], frequencies, width=np.diff(bin_edges), align='edge', edgecolor='black')
+    plt.title('Espectro de energia')
+    plt.xlabel('Energia (MeV)')
+    plt.ylabel('Intensidade')
     plt.show()
