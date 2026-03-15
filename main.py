39d9af357e6706a46da291fa7cd0280219b641e6
Ivan Ramos Pagnossin <puchacz.23@proton.me>
2026-03-15 19:49:32 -0300

    main: Np case

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from nd import run, build_and_plot_spectrum, plot_abundances
-from cases.eg_1 import energies, transitions
+# from cases.eg_1 import energies, transitions
+from cases.Np import energies, transitions
 
 
 if __name__ == "__main__":
-    last_states, energies = run(transitions, energies, 'A')
-    build_and_plot_spectrum(energies, 'Photon emission')
-    build_and_plot_spectrum(energies, 'Beta decay')
+    last_states, energies = run(transitions, energies, 'Np')
+    for process in energies.keys():
+        build_and_plot_spectrum(energies, process)
     plot_abundances(last_states)
