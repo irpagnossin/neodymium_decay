43d318a9a6943561736b89f388c63dd807687d2f
Ivan Ramos Pagnossin <puchacz.23@proton.me>
2026-03-15 19:49:19 -0300

    fix: do not fix x-range for energy histogram

--- nd/mc.py
+++ nd/mc.py
@@ -33,7 +33,7 @@ def build_and_plot_spectrum(
     import numpy as np
 
     energies_ = [-1 * energy for energy in energies[process]]
-    frequencies, bin_edges = np.histogram(energies_, bins=50, range=(0, 1.1), density=True)
+    frequencies, bin_edges = np.histogram(energies_, bins=50, density=True)
 
     plt.bar(bin_edges[:-1], frequencies, width=np.diff(bin_edges), align='edge', edgecolor='black')
     plt.title(f'Espectro de energia: {process}')
